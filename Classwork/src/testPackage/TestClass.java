a35e50fd9a6669db40068d3a3dfded86c3ef3402
s-jouv <s-jouv@51L104-16.it.bsd405.org>
2017-09-05 14:44:32 -0700

    5 times!

--- Classwork/src/testPackage/TestClass.java
+++ Classwork/src/testPackage/TestClass.java
@@ -5,5 +5,9 @@ public class TestClass
 public static void main (String[] args)
 	{
 	System.out.println("Hello World");
+	System.out.println("Hello Worl");
+	System.out.println("Hello Wor");
+	System.out.println("Hello Wo");
+	System.out.println("Hello W");
 	}
 }
