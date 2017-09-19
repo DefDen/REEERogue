d4ea67a2a1c4819f7b838d47ac93debea7db8007
s-jouv <s-jouv@51L104-07.it.bsd405.org>
2017-09-19 14:34:44 -0700

    Diamond

--- Classwork/src/testPackage/TestClass.java
+++ Classwork/src/testPackage/TestClass.java
@@ -2,12 +2,39 @@ package testPackage;
 
 public class TestClass 
 {
-public static void main (String[] args)
+	public static void main (String[] args)
 	{
-	System.out.println("Hello World");
-	System.out.println("Hello Worl");
-	System.out.println("Hello Wor");
-	System.out.println("Hello Wo");
-	System.out.println("Hello W");
+		for (int x = 1; x != 6; x++)
+		{
+		    for (int y = x; y <= 4; y++)
+		    {
+		        System.out.print("-");
+		    }
+		    for (int a = 1; a <= 2 * x - 1; a++)
+		    {
+		    	System.out.print(x);
+			}
+		    for (int y = x; y <= 4; y++)
+		    {
+		        System.out.print("-");
+		    }
+		    System.out.println();
+		}
+		for (int x = 4; x != 0; x--)
+		{
+		    for (int y = x; y <= 4; y++)
+		    {
+		        System.out.print("-");
+		    }
+		    for (int a = 1; a <= 2 * x - 1; a++)
+		    {
+		    	System.out.print(x);
+			}
+		    for (int y = x; y <= 4; y++)
+		    {
+		        System.out.print("-");
+		    }
+		    System.out.println();
+		}
 	}
 }
