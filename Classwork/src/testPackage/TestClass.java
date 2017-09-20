2a7cfa7026ed4b07d82a49a617d74001937c2bf8
s-jouv <s-jouv@51S50023276.it.bsd405.org>
2017-09-20 11:59:59 -0700

    Added ellipse function

--- Classwork/src/testPackage/TestClass.java
+++ Classwork/src/testPackage/TestClass.java
@@ -4,6 +4,51 @@ public class TestClass
 {
 	public static void main (String[] args)
 	{
+		ellipse();
+	}
+	public static void ellipse()
+	{
+		int y;
+		for (int x = 0; x <= 2; x++)
+		{
+			for (y = x ; y < 2; y++)
+			{
+				System.out.print("-");
+			}
+			System.out.print("o");
+			for (y = 3 - x ; y < 3; y++)
+			{
+				System.out.print("--");
+			}
+			System.out.print("o");
+			for (y = x ; y < 2; y++)
+			{
+				System.out.print("-");
+			}
+			System.out.println();
+		}
+		for (int x = 2; x >= 0; x--)
+		{
+			for (y = x ; y < 2; y++)
+			{
+				System.out.print("-");
+			}
+			System.out.print("o");
+			for (y = 3 - x ; y < 3; y++)
+			{
+				System.out.print("--");
+			}
+			System.out.print("o");
+			for (y = x ; y < 2; y++)
+			{
+				System.out.print("-");
+			}
+			System.out.println();
+		}
+	}
+	
+	public static void diamond()
+	{
 		for (int x = 1; x != 6; x++)
 		{
 		    for (int y = x; y <= 4; y++)
