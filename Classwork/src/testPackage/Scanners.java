aea4e03155090e7e8cdb07514ea21b0c178eec1a
s-jouv <s-jouv@51S50023276.it.bsd405.org>
2017-10-05 08:53:48 -0700

    More effecient use of scanners

--- Classwork/src/testPackage/Scanners.java
+++ Classwork/src/testPackage/Scanners.java
@@ -4,12 +4,11 @@ public class Scanners
 {
 	public static void main(String[] args)
 	{
-		Scanner name = new Scanner(System.in);
-		Scanner age = new Scanner(System.in);
+		Scanner input = new Scanner(System.in);
 		System.out.print("What is your name? ");
-		String strName = name.nextLine();
+		String strName = input.nextLine();
 		System.out.print("What is your age? ");
-		String strAge = age.nextLine();
+		String strAge = input.nextLine();
 		System.out.print("Hello " + strName + ". You are " + strAge + " years old.");
 	}
 }
