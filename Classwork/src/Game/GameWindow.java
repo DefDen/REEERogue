301bc9311de1b2c871ab43fe386b51e9323b26a7
s-jouv <s-jouv@51L104-05.it.bsd405.org>
2019-09-26 14:57:35 -0700

    JFrame can properly take user input and can display text properly. Next steps are to control where the elements I'm adding to the frame are placed and figuring out how to handle the player input I'm now receiving.

--- Classwork/src/Game/GameWindow.java
+++ Classwork/src/Game/GameWindow.java
@@ -1,11 +1,56 @@
 package Game;
 
+import java.awt.event.KeyListener;
+import java.util.Stack;
+import java.awt.Container;
+import java.awt.event.KeyEvent;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
 
 public class GameWindow 
 {
 	public GameWindow()
 	{
-		JFrame window = new JFrame();
+		JFrame window = new JFrame("REEERogue");
+		JLabel label = new JLabel("Test");
+		Container content = window.getContentPane();
+		JTextField text = new JTextField();
+		Stack<String> moveHistory = new Stack<String>();
+		KeyListener listener = new KeyListener()
+		{
+			@Override
+			public void keyPressed(KeyEvent event)
+			{
+				ifA(event.getKeyChar());
+				moveHistory.add("" + event.getKeyChar());
+				text.setText("");
+			}
+			@Override
+			public void keyReleased(KeyEvent event) 
+			{
+			}
+			@Override
+			public void keyTyped(KeyEvent event)
+			{
+			}
+		};
+		content.add(text);
+		content.add(label);
+		text.addKeyListener(listener);
+		window.pack();
+		window.setSize(300, 300);
+		window.setVisible(true);
+	}
+	
+	public void ifA(char x)
+	{
+		if(x == 'a')
+		System.out.println("It's a A");
+	}
+
+	public static void main(String args[])
+	{
+		GameWindow g = new GameWindow();
 	}
 }
