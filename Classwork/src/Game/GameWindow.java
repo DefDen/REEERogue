4dacacef265bb77fc5db4ed16cbaa5d8a34135f4
s-jouv <s-jouv@51L104-05.it.bsd405.org>
2019-09-27 14:58:35 -0700

    Organized the code a bit better so it's actually readable now. Beginning to work on file reading and displaying the level.

--- Classwork/src/Game/GameWindow.java
+++ Classwork/src/Game/GameWindow.java
@@ -1,8 +1,12 @@
 package Game;
 
 import java.awt.event.KeyListener;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 import java.util.Stack;
 import java.awt.Container;
+import java.awt.Dimension;
 import java.awt.event.KeyEvent;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -10,9 +14,14 @@ import javax.swing.JTextField;
 
 public class GameWindow 
 {
+	private JFrame window;
+	private static final int WINDOW_WIDTH = 300;
+	private static final int WINDOW_HEIGHT = 300;
+	private char[][] floor;
+	
 	public GameWindow()
 	{
-		JFrame window = new JFrame("REEERogue");
+		makeWindow();
 		JLabel label = new JLabel("Test");
 		Container content = window.getContentPane();
 		JTextField text = new JTextField();
@@ -39,7 +48,22 @@ public class GameWindow
 		content.add(label);
 		text.addKeyListener(listener);
 		window.pack();
-		window.setSize(300, 300);
+		window.setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
+	}
+	
+	private void loadLevel(File floorFile) throws FileNotFoundException
+	{
+		Scanner scan = new Scanner(floorFile);
+		for(int x = 0; scan.hasNextLine(); x++)
+		{
+			
+		}
+	}
+	
+	private void makeWindow()
+	{
+		window = new JFrame("REEERogue");
+		window.setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
 		window.setVisible(true);
 	}
 	
