4daca3f6d157091dbc358ab3faa8e43b21480331
s-jouv <s-jouv@51L104-05.it.bsd405.org>
2019-10-03 14:58:58 -0700

    Found out how to add multiple components to the JFrame without them stacking using a JPanel. It might not work once the interface becomes more complex, but for testing right now it will work. Moving on to reading and writing the floor text files and displaying them.

--- Classwork/src/Game/GameWindow.java
+++ Classwork/src/Game/GameWindow.java
@@ -5,31 +5,55 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 import java.util.Stack;
+import java.awt.BorderLayout;
 import java.awt.Container;
 import java.awt.Dimension;
+import java.awt.Panel;
 import java.awt.event.KeyEvent;
 
 import javax.swing.GroupLayout;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
 import javax.swing.JTextField;
+import javax.swing.SwingConstants;
 
 public class GameWindow 
 {
 	private JFrame window;
 	private static final int WINDOW_WIDTH = 300, WINDOW_HEIGHT = 300;
-	private char[][] floor;
+	private char[][] floor = new char[21][79];
+	private Stack<String> moveHistory = new Stack<String>();
 
 	public GameWindow()
 	{
 		makeWindow();
-		JLabel label = new JLabel("Test");
-		Container content = window.getContentPane();
-		GroupLayout layout = new GroupLayout(content);
-		layout.setAutoCreateGaps(true);
-		layout.setAutoCreateContainerGaps(true);
-		JTextField text = new JTextField();
-		Stack<String> moveHistory = new Stack<String>();
+		JLabel label = new JLabel("Test", SwingConstants.CENTER);
+		JLabel label2 = new JLabel("Test2", SwingConstants.CENTER);
+		label.setVerticalAlignment(SwingConstants.TOP);
+		JTextField text = makeJTextField();
+		JPanel panel = new JPanel(new BorderLayout());
+		window.add(panel);
+		panel.add(label, BorderLayout.PAGE_START);
+		panel.add(label2, BorderLayout.LINE_START);
+		panel.add(text, BorderLayout.PAGE_END);
+		window.pack();
+		window.setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
+	}
+
+	private void loadLevel(File floorFile) throws FileNotFoundException
+	{
+		Scanner scan = new Scanner(floorFile);
+		for(int x = 0; scan.hasNextLine(); x++)
+		{
+			floor[x] = scan.nextLine().toCharArray();
+		}
+	}
+	
+	private JTextField makeJTextField()
+	{
+		JTextField text = new JTextField("Is it a A?");
+		text.setBounds(100, 0, 25, 10);
 		KeyListener listener = new KeyListener()
 		{
 			@Override
@@ -49,23 +73,7 @@ public class GameWindow
 			}
 		};
 		text.addKeyListener(listener);
-		layout.setHorizontalGroup
-		(
-			layout.createSequentialGroup()
-			.addComponent(label)
-			.addComponent(text)
-		);
-		window.pack();
-		window.setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
-	}
-
-	private void loadLevel(File floorFile) throws FileNotFoundException
-	{
-		Scanner scan = new Scanner(floorFile);
-		for(int x = 0; scan.hasNextLine(); x++)
-		{
-
-		}
+		return text;
 	}
 
 	private void makeWindow()
